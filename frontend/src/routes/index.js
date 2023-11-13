05278384b41db421cae1063495a43a2b674b2803
Tam-NhuBh <tnhubh@gmail.com>
2023-11-13 23:30:13 +0700

    Sidebar

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,47 @@
+import { lazy } from 'react'
+
+// use lazy for better code splitting, a.k.a. load faster
+// const Dashboard = lazy(() => import('../pages/Homepage'))
+const Forms = lazy(() => import('../pages/Forms'))
+
+
+const routes = [
+  // {
+  //   path: '/homepage', // the url
+  //   component: Dashboard, // view rendered
+  // },
+  {
+    path: '/forms',
+    component: Forms,
+  },
+  // {
+  //   path: '/cards',
+  //   component: Cards,
+  // },
+  // {
+  //   path: '/charts',
+  //   component: Charts,
+  // },
+  // {
+  //   path: '/buttons',
+  //   component: Buttons,
+  // },
+  // {
+  //   path: '/modals',
+  //   component: Modals,
+  // },
+  // {
+  //   path: '/tables',
+  //   component: Tables,
+  // },
+  // {
+  //   path: '/404',
+  //   component: Page404,
+  // },
+  // {
+  //   path: '/blank',
+  //   component: Blank,
+  // },
+]
+
+export default routes
